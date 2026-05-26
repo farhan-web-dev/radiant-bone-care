10a014b5fe1afe3f9be22e84ff6b253c49e01f9b
farhan-web-de <farhansultan008@gmail.com>
2026-05-26 20:11:49 +0500

    add more section

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     tsconfigPaths(),
-    tanstackStart({
-      server: { entry: "server" },
-    }),
+    tanstackStart(),
     react(),
   ],
 });
